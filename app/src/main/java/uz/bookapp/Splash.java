55a0e7e8d300427dc6178e7ee236699c6db30524
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-12-21 12:42:42 +0500

    Splash is successfully added

---
+++ app/src/main/java/uz/bookapp/Splash.java
@@ -0,0 +1,21 @@
+package uz.bookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(Splash.this, MainActivity.class));
+            finish();
+        }, 2000);
+    }
+}
