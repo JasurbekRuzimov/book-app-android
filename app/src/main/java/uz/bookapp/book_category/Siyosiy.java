64d1a15f17758837aedee6bc72dff019496ba1be
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-12-21 12:43:03 +0500

    Siyosiy Activity is successfully added

---
+++ app/src/main/java/uz/bookapp/book_category/Siyosiy.java
@@ -0,0 +1,22 @@
+package uz.bookapp.book_category;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import uz.bookapp.R;
+
+public class Siyosiy extends AppCompatActivity {
+ImageView backHome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_siyosiy);
+        backHome=findViewById(R.id.backHome);
+        backHome.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
